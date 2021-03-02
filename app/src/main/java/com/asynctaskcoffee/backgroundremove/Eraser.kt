c72df88a01f4188bbf4f0562c0e1be5fbc3cf6b0
eGo <asynctaskcoffee@gmail.com>
2021-03-02 22:16:30 +0300

    Image background remove

---
+++ app/src/main/java/com/asynctaskcoffee/backgroundremove/Eraser.kt
@@ -0,0 +1,45 @@
+package com.asynctaskcoffee.backgroundremove
+
+import android.graphics.Bitmap
+import com.huawei.hms.mlsdk.MLAnalyzerFactory
+import com.huawei.hms.mlsdk.common.MLFrame
+import com.huawei.hms.mlsdk.imgseg.MLImageSegmentationAnalyzer
+import com.huawei.hms.mlsdk.imgseg.MLImageSegmentationScene
+import com.huawei.hms.mlsdk.imgseg.MLImageSegmentationSetting
+
+class Eraser {
+
+    companion object {
+        private var mAnalyzer: MLImageSegmentationAnalyzer
+
+        init {
+            val analyzerSetting = MLImageSegmentationSetting.Factory()
+                .setExact(true)
+                .setAnalyzerType(MLImageSegmentationSetting.BODY_SEG)
+                .setScene(MLImageSegmentationScene.FOREGROUND_ONLY)
+                .create()
+
+            MLAnalyzerFactory.getInstance().getImageSegmentationAnalyzer(analyzerSetting).also {
+                mAnalyzer = it
+            }
+        }
+
+        @JvmStatic
+        fun eraseAndReturnResult(bitmap: Bitmap, eraserListener: EraserListener?) {
+            val mlFrame = MLFrame.fromBitmap(bitmap)
+            mAnalyzer.asyncAnalyseFrame(mlFrame)
+                .addOnSuccessListener { returnImageSegmentation ->
+                    eraserListener?.onResultReady(returnImageSegmentation.foreground)
+                }
+                .addOnFailureListener { exception ->
+                    eraserListener?.onError(exception)
+                }
+        }
+    }
+
+}
+
+interface EraserListener {
+    fun onResultReady(bitmap: Bitmap?)
+    fun onError(exception: Exception?)
+}
---
+++ app/src/main/java/com/asynctaskcoffee/backgroundremove/EraserActivity.kt
@@ -0,0 +1,50 @@
+package com.asynctaskcoffee.backgroundremove
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class EraserActivity : AppCompatActivity(), EraserListener {
+
+    lateinit var result: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eraser)
+        initViews()
+    }
+
+    private fun erase() {
+        val bitmapInput = BitmapFactory.decodeResource(
+            resources,
+            R.drawable.human
+        )
+        Eraser.eraseAndReturnResult(
+            bitmapInput,
+            this@EraserActivity
+        )
+    }
+
+    private fun initViews() {
+        result = findViewById(R.id.result)
+    }
+
+    override fun onResultReady(bitmap: Bitmap?) {
+        result.setImageBitmap(bitmap)
+    }
+
+    override fun onError(exception: Exception?) {
+        Toast.makeText(this@EraserActivity, exception?.message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun eraseBackground(view: View) {
+        erase()
+    }
+}
